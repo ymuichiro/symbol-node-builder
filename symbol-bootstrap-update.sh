1160e2d51064276edd49a75b906c98c4004be3bf
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2023-03-05 17:30:16 +0900

    [add] initial commit

---
+++ symbol-bootstrap-update.sh
@@ -0,0 +1,6 @@
+cd ~
+cp ~/target/nodes/node/data/harvesters.dat ~/backup/harvesters.dat
+cp ~/target/preset.yml ~/backup/preset.yml 
+cp ~/target/addresses.yml ~/backup/addresses.yml
+
+npm install -g symbol-bootstrap@1.1.x
